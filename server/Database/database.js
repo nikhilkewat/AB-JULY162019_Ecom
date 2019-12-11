ebca098bd2446b528ba0dd355100529a1659aa5c
Nikhil Kewat <36764805+nikhilkewat@users.noreply.github.com>
2019-12-11 12:11:41 +0530

    Server Initial Commit

---
+++ server/Database/database.js
@@ -0,0 +1,14 @@
+var mysql = require("mysql");
+
+
+var connection = mysql.createPool({
+  host: "remotemysql.com",
+  user: "pX4Ly7PFZ6",
+  password: "mV7ydx6Sbz",
+  database: "pX4Ly7PFZ6",
+  port: 3306,
+  });
+
+module.exports.connection = connection;
+
+
